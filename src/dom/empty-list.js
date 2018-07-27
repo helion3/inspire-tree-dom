ca1682f841297247bb40f0d0c525098147d15962
Mike Botsko <botsko@gmail.com>
2018-07-27 12:03:33 -0700

    Replaces :first/:last/:only pseudo-selectors with classes

--- src/dom/empty-list.js
+++ src/dom/empty-list.js
@@ -2,7 +2,7 @@ import { Component } from 'inferno';
 
 export default class EmptyList extends Component {
     render() {
-        return (<ol><li className='leaf'>
+        return (<ol><li className='leaf first last only'>
             <span className='title icon icon-file-empty empty'>{ this.props.text }</span>
         </li></ol>);
     }
